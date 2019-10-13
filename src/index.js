c3fc19ac7c1277843d59e8bd3d303f41361f402d
shutnikPk <51722234+shutnikPk@users.noreply.github.com>
2019-10-13 22:03:36 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -5,10 +5,17 @@
  * @returns number of weeks needed for finish education
  */
 module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
+    focus , 
+    knowsProgramming,
+    config 
     ) {
-      return 0;
+
+      if (knowsProgramming){
+        return Math.ceil(800 / config[focus]);
+      } else {
+        return Math.ceil(1300 / config[focus]);
+      }
+      
   };
+
   
